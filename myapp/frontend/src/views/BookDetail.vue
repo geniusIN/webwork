0819a7e7e979790ddba6a698e4a782b8bb4c07fe
geniusIN <rlaghddls9309@gmail.com>
2025-05-08 22:54:59 +0900

    lappush

--- myapp/frontend/src/views/BookDetail.vue
+++ myapp/frontend/src/views/BookDetail.vue
@@ -1,88 +1,82 @@
 <template>
-  <!-- <div class="container">
-    <div class="row">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col" class="text-center table-primary">번호</th>
-            <td scope="col" class="text-center">{{ book.id }}</td>
-            <th scope="col" class="text-center table-primary">제목</th>
-            <td scope="col" class="text-center">
-              {{ book.title }}
-            </td>
-            <th scope="col" class="text-center table-primary">저자</th>
-            <td scope="col" class="text-center">{{ book.writer }}</td>
-          </tr>
+    <div class="container">
+        <div class="row">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <td>{{ book.value.id }}</td>
 
-          <tr>
-            <th colspan="2" class="text-center table-primary">등록일자</th>
-            <td colspan="4">{{ book.dateFormat }}</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td colspan="6" class="text-left" valign="top" height="200">
-              <pre
-                style="white-space: pre-wrap;
-                  border: none;
-                  background-color: white; ">{{ boardInfo.content }}</pre>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="6" class="text-center">
-              <button
-                class="btn btn-xs btn-info" @click="goToUpdateForm(boardInfo.id)">
-                수정
-              </button>
-              <button
-                class="btn btn-xs btn-info" @click="goToListForm()">
-                목록
-              </button>
-             
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                        <th>제목</th>
+                        <td>{{ book.value.title }}</td>
+
+                        <th>저자</th>
+                        <td>{{ book.value.writer }}</td>
+
+                        <th>출판사</th>
+                        <td>{{ book.value.publisher }}</td>
+                    </tr>
+
+                    <tr>
+                        <th>등록일자</th>
+                        <td>{{ book.value.dateFormat }}</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="6" class="text-left" valign="top" height="200">
+                            <pre style="white-space: pre-wrap; border: none; background-color: white">{{
+                                bookDetail.content
+                            }}</pre>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="6" class="text-center">
+                            <button class="btn btn-xs btn-info" @click="goToUpdateForm(boardInfo.id)">수정</button>
+                            <button class="btn btn-xs btn-info" @click="goToListForm()">목록</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-
-
-    <div class="row">
-
-    </div>
-    <div>
-      <CommentComp v-if="boardInfo.id" :boardId="boardInfo.id"/>
-    </div>
-  </div> -->
-
 </template>
 <script>
-// import axios from 'axios';
-// import {useRoute, useRouter} from "vue";
+import axios from "axios";
+import { useRoute, useRouter } from "vue-router";
+import { ref, onMounted, watch } from "vue";
 
-// const serchNo = "";
-// const boardInfo = {};
-// const router = userRouter();
-// const route = useRoute();
+const serchNo = "";
+const bookDetail = {};
+const router = useRouter();
+const route = useRoute();
+const book = ref({});
 
-// const getBoardInfo = async() => {
-//   let result = await axios.get(`api/board/${searchNo}`);
-//   boardInfo = result.data[0];
-// }
-// function goToUpdateForm(id){
-//   router.push({path: '/boardForm', query: {id : id}})
-// }
-// function goToListForm(){
-//   router.push('/board')
-// }
+const getBookDetail = async () => {
+    const getBookDetail = async () => {
+        const searchNo = route.query.id;
+        const result = await axios.get(`/api/book/${searchNo}`);
+        book.value = result.data[0];
+    };
+};
+function goToUpdateForm(id) {
+    router.push({ path: "/reviewForm", query: { id: id } });
+}
+function goToListForm() {
+    router.push("/book");
+}
 
-  
-// function onCreated(){
-//   this.searchNo = route.query.id;
-//   getBoardInfo();
-// }
-  
-  
-  
-  
+onMounted(() => {
+    if (route.query.id) {
+        getBookDetail();
+    }
+});
 
+watch(
+    () => route.query.id,
+    (id) => {
+        if (id) getBookDetail();
+    },
+    { immediate: true }
+);
 </script>
--- myapp/frontend/src/views/BooksView.vue
+++ myapp/frontend/src/views/BooksView.vue
@@ -1,62 +1,59 @@
 <template>
-<div class="container">
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th>No.</th>
-          <th>제목</th>
-          <th>저자</th>
-          <th>출판사</th>
-          <th>등록일자</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(book, i) in formattedBookList" :key="book.id" @click="goToDetail(book.id)">
-          <td>{{ book.id }}</td>
-          <td>{{ book.title }}</td>
-          <td>{{ book.writer }}</td>
-          <td>{{ book.publisher }}</td>
-          <td>{{ book.dateFormat }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  {{ route.fullPath }}
+    <div class="container">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>제목</th>
+                    <th>저자</th>
+                    <th>출판사</th>
+                    <th>등록일자</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(book, i) in formattedBookList" :key="book.id" @click="goToDetail(book.id)">
+                    <td>{{ book.id }}</td>
+                    <td>{{ book.title }}</td>
+                    <td>{{ book.writer }}</td>
+                    <td>{{ book.publisher }}</td>
+                    <td>{{ book.dateFormat }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    {{ route.fullPath }}
 </template>
 
 <script setup>
-import axios from 'axios';
-import {ref} from 'vue';
-import {useRouter, useRoute} from 'vue-router';
-import {onMounted, computed} from "vue";
+import axios from "axios";
+import { ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { onMounted, computed } from "vue";
 
 const router = useRouter();
 const route = useRoute();
 const bookList = ref([]);
 
 // 도서 목록 가져오기
-const getBookList = async() => {
-  let result = await axios.get('/api/book');
-  bookList.value = result.data;
-}
+const getBookList = async () => {
+    let result = await axios.get("/api/book");
+    bookList.value = result.data;
+};
 
 // 도서 목록 출력
-onMounted(()=>{
-  getBookList();
-})
+onMounted(() => {
+    getBookList();
+});
 
 // 날짜 포맷 위해 새로운 map 생성
-const formattedBookList = computed(() => 
-  bookList.value.map(book => ({
-    ...book,
-    dateFormat: book.update_date
-      ? new Date(book.update_date).toLocaleDateString('ko-KR')
-      : '날짜 없음'
-  }))
+const formattedBookList = computed(() =>
+    bookList.value.map((book) => ({
+        ...book,
+        dateFormat: book.update_date ? new Date(book.update_date).toLocaleDateString("ko-KR") : "날짜 없음",
+    }))
 );
 
-function goToDetail(){
-  router.push('/')
+function goToDetail() {
+    router.push("/bookDetail");
 }
-
 </script>
